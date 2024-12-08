d80e12dfbbe2860cbfa2e8e94d066f6ad7384335
losfair <zhy20000919@hotmail.com>
2024-12-08 11:26:46 +0000

    publish

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule Rezmq.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/losfair/rezmq"
+
   def project do
     [
       app: :rezmq,
       version: "0.1.0",
+      description: "Fast ZeroMQ bindings for Elixir",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: [
+        maintainers: ["Heyang Zhou"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => @source_url}
+      ]
     ]
   end
 
@@ -22,7 +30,8 @@ defmodule Rezmq.MixProject do
   defp deps do
     [
       {:rustler, "~> 0.35.0"},
-      {:benchee, "~> 1.0", only: :dev}
+      {:benchee, "~> 1.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
